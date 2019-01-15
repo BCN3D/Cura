bfdf71a0b945eaa6b484ee5f4be48656d85a156b
Eloi Pardo <eloipr94@gmail.com>
2019-01-15 10:01:03 +0100

    change cura documentation link

--- cura/CuraActions.py
+++ cura/CuraActions.py
@@ -38,7 +38,7 @@ class CuraActions(QObject):
         # Starting a web browser from a signal handler connected to a menu will crash on windows.
         # So instead, defer the call to the next run of the event loop, since that does work.
         # Note that weirdly enough, only signal handlers that open a web browser fail like that.
-        event = CallFunctionEvent(self._openUrl, [QUrl("http://www.bcn3dtechnologies.com/en/3d-printer/bcn3d-cura/")], {})
+        event = CallFunctionEvent(self._openUrl, [QUrl("http://www.bcn3dtechnologies.com/wp-content/themes/BCN3D/pdfs/BCN3D_Technologies_BCN3D_Cura_User_Manual.pdf")], {})
         Application.getInstance().functionEvent(event)
 
     @pyqtSlot()
