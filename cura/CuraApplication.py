734a1c5a320e915a4589efb27b43220a9f15247e
Eloi Pardo <eloipr94@gmail.com>
2019-02-08 13:01:25 +0100

    fix deletion of all nodes

--- cura/CuraApplication.py
+++ cura/CuraApplication.py
@@ -1124,8 +1124,8 @@ class CuraApplication(QtApplication):
 
             for node in nodes:
                 print_mode_enabled = self.getGlobalContainerStack().getProperty("print_mode", "enabled")
-                if print_mode_enabled:
-                    node_dup = self._print_mode_manager.getDuplicatedNode(node)
+                node_dup = self._print_mode_manager.getDuplicatedNode(node)
+                if print_mode_enabled and node_dup:
                     op.addOperation(RemoveNodesOperation(node_dup))
                 else:
                     op.addOperation(RemoveSceneNodeOperation(node))
