ad61975607193b8f07e8f7b7c2f60af889e3a011
Eloi Pardo <eloipr94@gmail.com>
2019-02-13 12:42:40 +0100

    only update variable when sigmaxr19 is added

--- plugins/FirstRun/FirstRun.py
+++ plugins/FirstRun/FirstRun.py
@@ -14,8 +14,8 @@ class FirstRun(Extension):
 
     def _onGlobalStackChanged(self):
         if Preferences.getInstance().getValue("cura/first_run"):
-            Preferences.getInstance().setValue("cura/first_run", False)
             global_stack = Application.getInstance().getGlobalContainerStack()
             if global_stack.getBottom().getId() == "bcn3dsigmaxr19":
+                Preferences.getInstance().setValue("cura/first_run", False)
                 message = Message("Latest Sigmax R19 printers come with 0.4mm nozzles installed. Make sure to select the correct hotend when preparing the print file.", title="Sigmax default hotends")
                 message.show()
