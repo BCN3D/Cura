c859486a74fe466dbcc91b83ce5a3bb4a14d5e92
Eloi Pardo <epardo@bcn3dtechnologies.com>
2019-10-29 13:22:38 +0100

    add python requests cacert path

--- cura_app.py
+++ cura_app.py
@@ -38,6 +38,7 @@ if not known_args["debug"]:
         os.makedirs(dirpath, exist_ok = True)
         sys.stdout = open(os.path.join(dirpath, "stdout.log"), "w", encoding = "utf-8")
         sys.stderr = open(os.path.join(dirpath, "stderr.log"), "w", encoding = "utf-8")
+        os.environ["REQUESTS_CA_BUNDLE"] = "cacert.pem"
 
 import platform
 import faulthandler
